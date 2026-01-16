845e7d45ed90f1fe0ace9aeb9884f6f6a691a3d0
Raniel <musyokiraniel@gmail.com>
2026-01-16 22:23:03 +0300

    Backend Authentication Fixes & Improvements (#15)
    * add and lock authentication dependancies Flask, SQLAlchemy, JWT, Bcrypt, CORS
    
    * update admin seeder to use newUser model with set_password and many to many roles
    
    * completed backend authentication with register, login, refresh, JWT roles, and errror fixes
    
    * add CORS support, JWT token expiration, and auto db.create_all() in app factory
    
    * chore: ignore db and pycache files

--- order-flow-backend/app/__init__.py
+++ order-flow-backend/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from config import SQLALCHEMY_DATABASE_URI, SECRET_KEY, JWT_SECRET_KEY
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 jwt = JWTManager()
 bcrypt = Bcrypt()
 
@@ -17,10 +19,16 @@ def create_app():
     app.config["SECRET_KEY"] = SECRET_KEY
     app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
 
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 900
+    app.config["JWT_REFRESH_TOKEN_EXPIRES"] = 604800 
+
     db.init_app(app)
+    bcrypt.init_app(app)
     jwt.init_app(app)
     bcrypt.init_app(app)
     CORS(app)
+
+    CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
     # Blueprints
     from app.routes.auth_routes import auth_bp
@@ -30,5 +38,8 @@ def create_app():
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(cart_bp, url_prefix="/api")
     app.register_blueprint(order_bp, url_prefix="/api")
+
+    with app.app_context():
+        db.create_all()
 
     return app
